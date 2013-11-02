368b8a031085ea3b1ad7a02b8913b54d74c979dd
Rosemary Sanchez <rsanchez@madebyuppercut.com>
2013-11-01 20:10:50 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amandrewstories::Application.config.secret_key_base = 'c619a8f46ffbc9a5ee635e6515b38799675898cefe974f0cb764f31bc050804ed22dac89632185473e099b057be65493a72f703eed7c80b8c1979155768faa87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Amandrewstories::Application.config.session_store :cookie_store, key: '_amandrewstories_session'
